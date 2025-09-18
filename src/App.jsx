67ff201397a97a70f17cccca0f2b9640bead45b9
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2025-09-18 19:22:46 +0600

    welcomeMessage component design

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import Glow from './components/Glow/Glow';
 import Navbar from './components/Navbar/Navbar';
+import WelcomeMessage from './components/WelcomeMessage/WelcomeMessage';
 
 function App() {
   return (
@@ -8,7 +9,9 @@ function App() {
       <div className="container mx-auto px-4 py-8 max-w-6xl">
         <Navbar />
         <Glow />
-        <main className="relative z-10"></main>
+        <main className="relative z-10">
+          <WelcomeMessage />
+        </main>
       </div>
     </>
   );
---
+++ src/components/WelcomeMessage/WelcomeMessage.jsx
@@ -0,0 +1,9 @@
+export default function WelcomeMessage() {
+  return (
+    <>
+      <h2 className="text-4xl font-bold mb-8">
+        Let's create a masterpiece, Alvian! <span className="text-2xl">👋</span>
+      </h2>
+    </>
+  );
+}

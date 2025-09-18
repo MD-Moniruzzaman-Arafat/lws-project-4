595b83f793e2ad32eee9b432d282b4369d668c07
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2025-09-18 19:15:17 +0600

    navbar component design

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+
+function App() {
+  return (
+    <>
+      <div className="container mx-auto px-4 py-8 max-w-6xl">
+        <Navbar />
+      </div>
+    </>
+  );
+}
+
+export default App;
